f4265fef6f0790b9c7c1db510e163ae52c932683
Iker <ikercsv@gmail.com>
2023-04-23 12:47:06 -0600

    Últimos cambios, prototipo de robot atacante estable

--- Arduino/Examples/robot_atacante/robot_atacante.ino
+++ Arduino/Examples/robot_atacante/robot_atacante.ino
@@ -14,11 +14,11 @@
 #define OLED_RESET 4
 
 #define maxSpeed 255
-#define aproachSpeed 150
-#define angleSpeed 75
-#define rotationSpeed 50
-#define positionSpeed 25
-
+#define aproachSpeed 225
+#define angleSpeed 150
+#define rotationSpeed 75
+#define positionSpeed 50
+#define lowestSpeed 35
 #define COMPASS_DEVIATION 15
 
 Motor motor;
@@ -48,7 +48,7 @@ void setup() {
   qrd.attachMotors(4, 34, 35, 3, 33, 32, 2, 30, 31, 5, 36, 37);
   motor.attachUtils(53, 320, 240);
 
-  motor.reset(1500);  // Wait for NAVX to turn on TODO: Rewrite this function
+  motor.reset(2500);  // Wait for NAVX to turn on TODO: Rewrite this function
 };
 
 int j = 0;
@@ -88,7 +88,7 @@ void loop() {
     switch (preciseLocation) {
       case 0:
       case 1:
-        motor.TurnLeft(positionSpeed);
+        motor.TurnLeft(lowestSpeed);
         break;
       case 2:
       case 3:
@@ -99,10 +99,10 @@ void loop() {
         break;
       case 5:
       case 6:
-        motor.TurnRight(positionSpeed);
+        motor.TurnRight(lowestSpeed);
         break;
       case -1:
-        motor.TurnRight(rotationSpeed);
+        motor.TurnRight(lowestSpeed);
     }
   }
 
@@ -113,9 +113,9 @@ void loop() {
     oled.print(30, 0, angle, 1);
     oled.show();
 
-    if (angle >= 30) step = 2;
-    else if (angle <= 0 && angle <= -30) step = 3;
-    else step = 4;
+    if (angle >= 30) step = 2; // Ball between 180 and 30 degrees
+    else if (angle <= 0 && angle >= -30) step = 4; // Ball between 0 and -30 degrees
+    else step = 3; // Ball between -30 and -180 degrees
   }
 
   if (step == 2) {
@@ -132,7 +132,7 @@ void loop() {
       case 1:
       case 2:
       case 3:
-        motor.TurnLeft(positionSpeed);
+        motor.TurnLeft(lowestSpeed);
         break;
       case 4:
       case 5:
@@ -144,14 +144,14 @@ void loop() {
           motor.moveToAngle(0, maxSpeed, 1000);
           resetVariables();
         } else {
-          motor.moveToAngle(45, angleSpeed);
+          motor.moveToAngle(60, angleSpeed);
         }
         break;
       case 9:
       case 10:
       case 11:
       case 12:
-        motor.TurnRight(positionSpeed);
+        motor.TurnRight(lowestSpeed);
         break;
       case -1:
         motor.North(maxSpeed, 1000);
@@ -174,7 +174,7 @@ void loop() {
       case 1:
       case 2:
       case 3:
-        motor.TurnLeft(positionSpeed);
+        motor.TurnLeft(lowestSpeed);
         break;
       case 4:
       case 5:
@@ -186,14 +186,14 @@ void loop() {
           motor.moveToAngle(0, maxSpeed, 1000);
           resetVariables();
         } else {
-          motor.moveToAngle(315, angleSpeed);
+          motor.moveToAngle(300, angleSpeed);
         }
         break;
       case 9:
       case 10:
       case 11:
       case 12:
-        motor.TurnRight(positionSpeed);
+        motor.TurnRight(lowestSpeed);
         break;
       case -1:
         motor.North(maxSpeed, 500);
@@ -203,10 +203,46 @@ void loop() {
     }
   }
 
-  if (step == 4) {
+  if (step == 4) { // Positioning
+    ocx = camera.xOrange();
+    oled.print(95, 0, "S4", 1);
+    oled.print(105, 54, i, 1);
+    oled.print(0, 20, "ocx: ", 1); oled.print(30, 20, ocx, 1);
+    oled.show();
+
+    switch (utils.checkPreciseDirection(ocx, 13)) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+        motor.TurnLeft(lowestSpeed);
+        break;
+      case 5:
+      case 6:
+      case 7:
+        motor.hardReset();
+        angle = compass.checkAngle();
+        step = 5;
+        break;
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+      case 12:
+        motor.TurnRight(lowestSpeed);
+        break;
+      case -1:
+        motor.hardReset();
+        resetVariables();
+        break;
+    }
+  }
+
+  if (step == 5) {
     ocx = camera.xOrange();
 
-    oled.print(95, 0, "S4", 1);
+    oled.print(95, 0, "S5", 1);
     oled.print(105, 54, i, 1);
     oled.print(0, 20, "ocx: ", 1); oled.print(30, 20, ocx, 1);
     oled.print(0, 40, "deg: ", 1); oled.print(30, 40, angle, 1);
